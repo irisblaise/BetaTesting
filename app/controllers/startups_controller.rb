bf6110f000c15b297a176aabaaa9e3705c46417b
Iris van Dijken <irisvdijken@gmail.com>
2020-03-02 14:55:23 +0100

    created the CRUD for startup

---
+++ app/controllers/startups_controller.rb
@@ -0,0 +1,49 @@
+class StartupsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+    def index
+      @startups = Startup.all
+    end
+
+    def show
+      @startup = Startup.find(params[:id])
+    end
+
+    def new
+      @startup = Startup.new
+    end
+
+    def create
+      @startup = Startup.new(startup_params)
+      @startup.user = current_user
+      if @startup.save
+        #redirect_to dashboard_owner_path(@startup.user)
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @startup = Startup.find(params[:id])
+
+    end
+
+    def update
+      @startup = Startup.find(params[:id])
+      @startup.update(startup_params)
+      @startup.save
+      # redirect_to car_path(@startup)
+    end
+
+    def destroy
+      @startup = Startup.find(params[:id])
+      @startup.destroy
+      # redirect_to dashboard_owner_path
+    end
+
+    private
+
+    def startup_params
+      params.require(:startup).permit(:company_name, :url, :description, :sector)
+    end
+end
