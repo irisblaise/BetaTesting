8fa4d89c1e1cb578f52d673ff77bd996f010dd41
Tereza Valerova <terezavalerova@gmail.com>
2020-03-03 11:17:18 +0100

    authentication adjusted, seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,32 @@ Startup.destroy_all
 Tester.destroy_all
 User.destroy_all
 
-# puts "Creating users..."
-# puts "Creating startups..."
+puts "Creating users..."
+puts "Creating startups..."
 
-# 10.times do
-#   user = User.create(
-#     email: Faker::Internet.email,
-#     password: 'password',
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name
-#   )
+10.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: 'password',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
 
-#   startup = Startup.new(
-#     company_name: Faker::Company.name,
-#     description: Faker::Company.catch_phrase,
-#     sector: SECTOR.sample,
-#     url: Faker::Internet.url,
-#     user_id: user.id
-#   )
+  startup = Startup.new(
+    company_name: Faker::Company.name,
+    description: Faker::Company.catch_phrase,
+    sector: SECTOR.sample,
+    url: Faker::Internet.url,
+    user_id: user.id
+  )
 
 
-#   photo_url = ['https://images.pexels.com/photos/1482061/pexels-photo-1482061.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://images.pexels.com/photos/267399/pexels-photo-267399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'].sample
-#   file = URI.open(photo_url)
-#   startup.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-#   startup.save
+  photo_url = ['https://images.pexels.com/photos/1482061/pexels-photo-1482061.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://images.pexels.com/photos/267399/pexels-photo-267399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'].sample
+  file = URI.open(photo_url)
+  startup.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  startup.save
 
-# end
+end
 
 puts "Creating testers..."
 
