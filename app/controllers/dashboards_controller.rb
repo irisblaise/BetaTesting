aa025d4c5b2c3e818d1efa521baa61610228e340
Iris van Dijken <irisvdijken@gmail.com>
2020-03-04 17:50:08 +0100

    dashboard startup

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,9 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @startup = Startup.find_by(user: current_user)
     @version = Version.new
   end
+
+
 end
