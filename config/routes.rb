8a4d8190b5e2f0b4d1372e1f8ae240aefee31edb
theogilardo <gilardotheo@gmail.com>
2020-03-04 16:21:56 +0100

    Save css shx

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
 
-
-
   devise_for :users #, :controllers => {:registrations => "registrations"}
   root to: 'pages#home'
   get "/dashboard", to: "dashboards#show"
 
 
-  resources :testers, only: [:new, :create, :edit, :update, :delete]
+  resources :testers
   resources :startups do
       resources :versions do
         resources :feedbacks
