e1ca618ac622b348bec2e123f19e518316f9c65d
theogilardo <gilardotheo@gmail.com>
2020-03-09 16:24:23 +0100

    Save configs charts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_03_09_085540) do
-=======
+
 ActiveRecord::Schema.define(version: 2020_03_09_090427) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
