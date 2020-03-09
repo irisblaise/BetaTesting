74ad9cc08f5e93cb9333f4b8d4d477b87e8677b5
Tereza Valerova <terezavalerova@gmail.com>
2020-03-09 10:57:31 +0100

    reviews controller created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,11 +4,21 @@ class ReviewsController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def index
+    @reviews = Review.all
   end
 
   def new
+    @tester = Tester.find(params[:tester_id])
   end
 
   def create
   end
+
+
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :tester_id)
+  end
+
 end
