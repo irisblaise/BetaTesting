09cfb6fdf868e473a36c8483611ffbedbaebba55
theogilardo <gilardotheo@gmail.com>
2020-03-09 14:26:09 +0100

    about us rooter

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users #, :controllers => {:registrations => "registrations"}
   root to: 'pages#home'
   get "/dashboard", to: "dashboards#show"
+  get "/about_us", to: 'pages#about_us'
+
 
   resources :testers
   resources :startups do
