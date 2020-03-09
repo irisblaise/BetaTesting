284ab441012f37f95c9a93e126f0cdabfda4acb4
Tereza Valerova <terezavalerova@gmail.com>
2020-03-09 15:51:51 +0100

    js file added to create toggle between views in startup dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,10 @@ import 'bootstrap';
 
 import { activateSearchBar } from './searchBar'
 import { newQuestion, deleteQuestion } from "./version"
+import { myFunction} from './toggle'
 
 
 activateSearchBar()
 newQuestion()
 deleteQuestion()
+myFunction()
