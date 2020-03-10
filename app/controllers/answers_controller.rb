7e2976c90ac8e72921f5259d9a54cd0baebeb42f
Tereza Valerova <terezavalerova@gmail.com>
2020-03-10 18:30:26 +0100

    changes to answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,17 +4,21 @@ class AnswersController < ApplicationController
   def create
     all_answers = []
 
-    params[:questions].each_with_index do |question_id, index|
-      @question = Question.find(question_id)
-      @version = @question.version
-      @feedback = Feedback.find_or_create_by! tester_id: current_user.tester.id, version_id: @version.id
+    @version = Version.find(params[:version_id])
+    @feedback = Feedback.find_or_create_by! tester_id: current_user.tester.id, version_id: @version.id
 
-      all_answers.push({
-        feedback_id: @feedback.id,
-        question_id: question_id,
-        answer: params[:answers][index]
-      })
+    if params[:questions]
+      params[:questions].each_with_index do |question_id, index|
+        @question = Question.find(question_id)
+
+        all_answers.push({
+          feedback_id: @feedback.id,
+          question_id: question_id,
+          answer: params[:answers][index]
+        })
+      end
     end
+
     authorize @feedback.tester
     @feedback.update(feedback_params) if params[:feedback]
     @feedback.save
