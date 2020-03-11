7a7b82212db1d57d7492f6432b5c7cb78f493549
Tereza Valerova <terezavalerova@gmail.com>
2020-03-11 11:55:51 +0100

    default value added to feedback columns

--- db/migrate/20200311105209_add_defaults_to_feedback_columns.rb
+++ db/migrate/20200311105209_add_defaults_to_feedback_columns.rb
@@ -3,9 +3,9 @@ class AddDefaultsToFeedbackColumns < ActiveRecord::Migration[5.2]
     # do the below when creating a new column
     # add_column :table, :column, :type, default: 0, nil: 0
     change_column :feedbacks, :website_ux, :integer, default: 0, nil: 0
-    change_column :feedbacks, :website_ux, :integer, default: 0, nil: 0
-    change_column :feedbacks, :website_ux, :integer, default: 0, nil: 0
-    change_column :feedbacks, :website_ux, :integer, default: 0, nil: 0
-    change_column :feedbacks, :website_ux, :integer, default: 0, nil: 0
+    change_column :feedbacks, :website_ui, :integer, default: 0, nil: 0
+    change_column :feedbacks, :website_design, :integer, default: 0, nil: 0
+    change_column :feedbacks, :website_fluidity, :integer, default: 0, nil: 0
+    change_column :feedbacks, :website_latency, :integer, default: 0, nil: 0
   end
 end
