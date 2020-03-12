9e0645a6cd5dff4e0e32eafd73f0d858eea41f01
Tereza Valerova <terezavalerova@gmail.com>
2020-03-12 09:25:56 +0100

    set up done to allow anyone to see about, contact and all startups pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-   skip_before_action :authenticate_user!, only: [:home]
+   skip_before_action :authenticate_user!, only: [:home, :about_us, :help_and_support]
 
   def home
+  end
+
+  def about_us
+
+  end
+
+  def help_and_support
+
   end
 end
